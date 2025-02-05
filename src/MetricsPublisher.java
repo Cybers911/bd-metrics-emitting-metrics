a7de39cf76b495335c954fa9bf50dd5b2d70047f
Cybers911 <info@cybers911.com>
2025-02-04 23:12:24 -0600

    sol1

--- src/MetricsPublisher.java
+++ src/MetricsPublisher.java
@@ -1,6 +1,8 @@
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
 import com.amazonaws.services.cloudwatch.AmazonCloudWatchClientBuilder;
+import com.amazonaws.services.cloudwatch.model.Dimension;
+import com.amazonaws.services.cloudwatch.model.MetricDatum;
 import com.amazonaws.services.cloudwatch.model.PutMetricDataRequest;
 import com.amazonaws.services.cloudwatch.model.StandardUnit;
 
@@ -34,8 +36,35 @@ public class MetricsPublisher {
      */
     public PutMetricDataRequest buildMetricDataRequest(final String metricName, final double value,
                                                         final StandardUnit unit) {
+        Dimension dimension = new Dimension()
+                // Replace "ENVIRONMENT" with the actual dimension name
+               .withName("ENVIRONMENT")
+                // Replace "PRODUCTION" with the actual dimension value
+               .withValue("PRODUCTION");
+
+        MetricDatum datum = new MetricDatum()
+                // Replace "MyCustomMetric" with the actual metric name
+               .withMetricName(metricName)
+                // Replace 123.45 with the actual value
+               .withValue(value)
+                // Replace "Seconds" with the actual unit
+               .withUnit(unit);
+               datum.withDimensions(dimension);
+
+                PutMetricDataRequest request = new PutMetricDataRequest()
+                        // Replace "EXAMPLE/ORDERS" with the actual namespace
+               .withNamespace("EXAMPLE/ORDERS")
+                        // Add the created MetricDatum to the request
+               .withMetricData(datum);
+
+                return request;
+
 
         // TODO: implement
-        return null;
+
+
+
+
+
     }
 }
